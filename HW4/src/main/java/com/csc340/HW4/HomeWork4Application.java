deeddc6ccd3c397988a2a915db16a3764624e484
TTQBqueen <156357678+TTQBqueen@users.noreply.github.com>
2024-10-08 17:51:34 -0400

    Add files via upload

---
+++ HW4/src/main/java/com/csc340/HW4/HomeWork4Application.java
@@ -0,0 +1,13 @@
+package com.csc340.HW4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork4Application.class, args);
+	}
+
+}
